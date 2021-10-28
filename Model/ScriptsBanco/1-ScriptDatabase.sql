dfe1ca6e8e708875c4edf8c353380fb34aa5c4e9
Rodrigo Cavalcanti de Castro <rodrigo.darlane@gmail.com>
2021-10-28 19:02:00 -0300

    Delete Model directory

--- Model/ScriptsBanco/1-ScriptDatabase.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE "BaseVendas"
-    WITH 
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'C'
-    LC_CTYPE = 'C'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
